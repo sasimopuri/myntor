f813b01aa7321e3c5c820876bccdce00436597b2
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2024-01-28 19:28:22 +0530

    App.jsx -> Added signup page path

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Quiz from "./Quiz";
 
 const App = () => {
   return (
-    <div className="p-2 m-6 border border-purple-300 shadow-md rounded-md bg-purple-200 flex flex-col gap-1 justify-center items-center">
+    // <div className="p-2 m-6 border border-purple-300 shadow-md rounded-md bg-purple-200 flex flex-col gap-1 justify-center items-center">
       <Outlet />
-    </div>
+    // </div>
   );
 };
 

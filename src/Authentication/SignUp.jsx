72c63b222c6adcbaedf925ab4dc62d368151afbe
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2024-01-28 19:46:47 +0530

    signup -> added email and onChange function,increased btn size

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -1,17 +1,59 @@
+import { useEffect, useState } from "react";
 import "./SignUp.css";
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleOnFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="form-container">
       <h1 className="heading">Register</h1>
-      <form action="" className="signup-form">
-        <label htmlFor="first_name">First Name :</label>
-        <input type="text" name="first_name" id="first_name" />
-        <label htmlFor="last_name">Last Name :</label>
-        <input type="text" name="last_name" id="last_name" />
+      <form onSubmit={handleOnFormSubmit} className="signup-form">
+        <label htmlFor="name">Name :</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={(e) => {
+            handleOnChange(e);
+          }}
+        />
+        <label htmlFor="email">Email :</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={(e) => {
+            handleOnChange(e);
+          }}
+        />
         <label htmlFor="password">Password :</label>
-        <input type="text" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={(e) => {
+            handleOnChange(e);
+          }}
+        />
         <div className="btn-submit">
-          <div className="form-submit">Submit</div>
+          <button className="form-submit">Submit</button>
         </div>
       </form>
     </div>
